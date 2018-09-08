b74bf7ca450082e9570be5956928a04b827ee1a7
Ykaro237 <tiagonegri237@gmail.com>
2018-09-08 17:09:20 -0300

    verification error (md5 based) were implemented in the message class

--- TP1/src/message/Message.java
+++ TP1/src/message/Message.java
@@ -7,6 +7,11 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.security.*;
+
+import com.sun.javafx.runtime.VersionInfo;
+
+import java.math.*;
 
 public class Message{
     private long seq_number;               // número de sequencia da mensagem (unsigned 64 bits )
@@ -20,6 +25,18 @@ public class Message{
         this.seq_number = seq_number;
         this.message = message;
         this.message_size = message.length();
+    }
+
+    private void calculate_verification_error(){
+        String verification_string = String(this.seq_number) + String(time_sent) + String(time_stamp);
+        verification_string += String(message_size) + String(message) + String(verification_code);
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.update(s.getBytes(),0,s.lenght);
+        this.verification_code = new BigInteger(1,m.digest());
+    }
+
+    private void get_message_times(){
+
     }
 
     public long get_sequential_number(){
@@ -41,6 +58,8 @@ public class Message{
         return this.verification_code;
     }
     public void on_send(){
-        
+        get_message_times();
+        calculate_verification_error();
     }
+
 }
