a9875d1c0681403b01f22cc624aaad3002a18ec2
Ykaro237 <tiagonegri237@gmail.com>
2018-09-12 12:09:32 -0300

    window functions were implemented

--- TP1/src/message/Window.java
+++ TP1/src/message/Window.java
@@ -11,31 +11,63 @@ import message.Message;
 
 public class Window{
     private Message[] message_list;     // lista de mensagens na janela
+    private Boolean[] message_state;    // lista de confirmação de mensagens
     private int rws;                    // tamanho da janela
     private int lfa;                    // ultimo quadro aceitavel (last frame accepted)
     private int nfe;                    // proximo quadro aceitavel (next frame accepted)
 
     public Window(int rws){
         this.message_list = new Message[rws];
+        this.message_state = new Boolean[rws];
         this.rws = rws;
-        this.lfa = rws - 1;
+        this.lfa = 0;
         this.nfe = 0;
     }
 
     public Message get_next_message(){
         return this.message_list[nfe];
     }
+    
     public int get_next_message_index(){
         return this.nfe;
     }
-    public void add_new_message(Message new_message){
-
+    
+    public int get_last_message_index(){
+        return this.lfa;
     }
+    
+    public int get_window_size(){
+        return this.rws;
+    }
+
+    public Boolen is_empty(){
+        if(lfa == nfe){
+            return true;
+        }
+        else
+            return false;
+    }
+
     public Boolean is_full(){
-        return false;
+        if( (lfa + 1)%rws == nfe)
+            return true;
+        else
+            return false;
     }
 
-    public void message_received(Message received_message){
-
+    public void receive_message(Message received_message){
+        this.lfa = (this.lfa + 1)%this.rws;
+        this.message_list[lfa] = received_message;
+        this.message_state[lfa] = false;
     }
+
+    public void confirm_message(long message_seq_number){
+        for(int i = this.nfe; i != this.lfa; i = (i+1)%this.rws){
+            if(this.message_list[i].get_sequential_number() == message_seq_number){
+                this.message_state[i] = true;
+            }
+            if(this.message_state[i] && i == nfe){
+                this.nfe = (this.nfe + 1)%this.rws;
+            }
+        }
 }
